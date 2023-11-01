d52a1ab80f4879d6f13370541021d6285794a44c
Ainoko Alex Idachaba <click2alex@gmail.com>
2023-11-01 09:52:47 +0100

    version 3 added

--- Jenkinsfile-cred-groovy-script/script.groovy
+++ Jenkinsfile-cred-groovy-script/script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh "docker build -t alexdevops31/jenkins-demo-app:jda-2.0 ."
+    sh "docker build -t alexdevops31/jenkins-demo-app:jda-3.0 ."
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh "docker push alexdevops31/jenkins-demo-app:jda-2.0"
+    sh "docker push alexdevops31/jenkins-demo-app:jda-3.0"
     }
 } 
 
